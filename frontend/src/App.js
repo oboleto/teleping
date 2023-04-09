6570551f0d7da6672564238e8868c6a03eb21be3
Bytter <67333597+Bytters@users.noreply.github.com>
2023-04-09 17:43:03 -0300

    twitch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 function App() {
   return (
     <article>
-      <a href="
-      https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=ucvnf3or3v9qrd1rufrsjt6jjwi6xk&redirect_uri=https://teleping.boletinho.com/&scope=&state=">Login with Twitch</a>
+      <a href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=ucvnf3or3v9qrd1rufrsjt6jjwi6xk&redirect_uri=https://teleping.boletinho.com/&scope=&state=">Login with Twitch</a>
     </article>
   );
 }
